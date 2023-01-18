f5bb07586a0264ecc47216d7f40449fdb3b0ae87
Gelo Saldana <geloalex@gmail.com>
2023-01-18 13:19:39 -0400

    Initial commit

---
+++ Topics/Integer types and operations/Reversing input numbers/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        String str = "GeloAlex";
+        System.out.println(str.charAt(5));
+        System.out.println(str.substring(0,3));
+       // System.out.println(str[5]);
+     //   System.out.println(str.get(5));
+    }
+}
